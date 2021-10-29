3c9299f9528bc6fa9cafeadaca1e4aed4f639042
EmilyDSarani <emily.sarani@gmail.com>
2021-10-29 17:17:26 -0500

    hypothetically it is all working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,4 +6,16 @@ describe('app routes', () => {
     const response = await request (app).get('/hi');
     expect(response.text).toEqual('hi');
   });
+  it('should get back the /hi route with GET', async() => {
+    const response = await request (app).get('/red');
+    expect(response.text).toEqual('<h1> Red </h1>');
+  });
+  it('should get back the /hi route with GET', async() => {
+    const response = await request (app).get('/blue');
+    expect(response.text).toEqual('<h1> Blue </h1>');
+  });
+  it('should get back the /hi route with GET', async() => {
+    const response = await request (app).get('/green');
+    expect(response.text).toEqual('<h1> Green </h1>');
+  });
 });

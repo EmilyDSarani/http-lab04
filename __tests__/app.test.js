14c1c178279e7a30455837ad0d8af1fc50834762
EmilyDSarani <emily.sarani@gmail.com>
2021-10-29 17:06:43 -0500

    got the hi route working and passing. This lab is finally making a lot more sense

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,8 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
-
+  it('should get back the /hi route with GET', async() => {
+    const response = await request (app).get('/hi');
+    expect(response.text).toEqual('hi');
+  });
 });
